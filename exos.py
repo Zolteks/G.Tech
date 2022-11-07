c38c51d2e2957d655ea58cd4ac1cd339d60a0633
Zolteks <arthus@meuret.net>
2022-11-07 18:58:44 +0100

    functions

--- exos.py
+++ exos.py
@@ -17,4 +17,25 @@ assertionA = ((365 * 3) / (4 - (12 - 8)) * (rh) > r) #False
 assertionB = (e * s < r) #False
 assertionFinal = assertionA == assertionB  #True
 
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def modulo(x, y):
+    return x % y
+
+def salaireNet(brut, coefficient):
+    return brut - (brut*coefficient)
+
+def salaireParSeconde(salaireHoraire, heureParJourOuvré, nbJourOuvréParAn):
+    return nbJourOuvréParAn * heureParJourOuvré * (salaireHoraire / 3600)
+
 # Fin
