0e376ceac46064f05b6c78cc37a3ffd8a7945995
Zolteks <arthus@meuret.net>
2022-11-10 11:35:56 +0100

    ajustement

--- fibo.py
+++ fibo.py
@@ -1,11 +1,17 @@
 import sys
 sys.setrecursionlimit(2000)
 
-def fibo(n, suite=[0,1]):
+def fiboRecursif(n, suite=[0,1]):
     l = len(suite)
-    suite.append(suite[l-1] + suite[l-2])
+    suite.append(suite[-1] + suite[-2])
     if l == n:
         return suite[l-1]
     return fibo(n, suite)
+
+def fibo(n):
+    suite = [0, 1]
+    while len(suite) - 1 != n:
+        suite.append(suite[-1] + suite[-2])
+    return suite[-1]
 
 print(fibo(1000))
--- pfcFinal.py
+++ pfcFinal.py
@@ -4,21 +4,22 @@
 # on admet la fonction input() qui renvoie la valeur donné par l'utilisateur.
 
 # definir une fonction pfc() qui lance une partie de pierre feuille ciseau.
-    # initialiser scoreJoueur et scoreIA, les variables de score des deux joueurs avec la valeur 0.
+    # définir scoreJoueur avec la valeur 0.
+    # définir scoreIA avec la valeur 0.
     # tant que scoreJoueur est different de 3 ou scoreIA est different de 3.
-        # assigner à la variable choixJoueur la valeur du retour de la fonction input("choisissez une action : 1 pour feuille, 2 pour pierre, 3 pour ciseau").
-        # assigner à la variable choixIA la valeur du retour de la fonction randint() avec comme parametres 1 et 3.
+        # assigner à la variable choixJoueur la valeur du retour de l'execution de la fonction input("choisissez une action : 1 pour feuille, 2 pour pierre, 3 pour ciseau").
+        # assigner à la variable choixIA la valeur du retour de l'execution de la fonction randint() avec comme parametres 1 et 3.
 
         # si choixJoueur est égal choixIA.
             # alors on écrit "égalité"
 
         # sinon si choixJoueur égal 1 et choixIA égal 2 ou choixJoueur égal 2 et choixIA égal 3
             # alors on écrit "victoire"
-            # alors on incrémente scoreJoueur
+            # alors on incrémente scoreJoueur de 1
 
         # sinon si choixJoueur égal 2 et choixIA égal 1 ou choixJoueur égal 3 et choixIA égal 2
             # alors on écrit "défaite"
-            # alors on incrémente scoreIA
+            # alors on incrémente scoreIA de 1
 
         # on écrit "scores : {scoreJoueur} / {scoreIA}"
 
