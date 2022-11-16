588d4ed55d402d50f7a89addddf73af84f170e6a
Zolteks <arthus@meuret.net>
2022-11-16 09:06:49 +0100

    Cortana v.2

--- morpionImpossible.py
+++ morpionImpossible.py
@@ -5,7 +5,7 @@ def impossibleMorpion():
     tab = [ ["_" for i in range(3)] for i in range(3) ]
     curPlayer = {
         1: "Joueur",
-        -1: "IA"
+        -1: "Cortana"
     }
     curPlayerID = 1
     playerSymbole = {
@@ -14,7 +14,7 @@ def impossibleMorpion():
     }
 
     tab[1][1] = "X"
-    action = 0
+    action = 1
 
 
     while True:
@@ -28,45 +28,70 @@ def impossibleMorpion():
             choix = False
             while not choix:
                 print("C'est à toi de jouer !")
-                choixX = int(input("coordonné x : "))
-                choixY = int(input("coordonné y : "))
+                choixY = int(input("coordonné x : "))
+                choixX = int(input("coordonné y : "))
                 if tab[choixX][choixY] != "_":
                     print("case occupée, fais un autre choix")
                 else:
                     tab[choixX][choixY] = playerSymbole[curPlayerID]
                     choix = True
         else:
-            print("Au tour de l'IA")
+            print("Au tour de Cortana")
 
-        if action == 2:
+        print("")
+        if action == 3 and curPlayerID == -1:
             if tab[0][0] == "_":
                 tab[0][0] = "X"
             else:
                 tab[0][2] = "X"
         
-        if action == 4:
+        if action == 5 and curPlayerID == -1:
+            check = False
             for x in range(3):
-                for y in range(3):
-                    if tab[x][y] == "_":
-                        tempTab = copy.deepcopy(tab)
-                        tempTab[x][y] = "O"
-                        if (tempTab[0][0] == "O" and tempTab[0][1] == "O" and tempTab[0][2] == "O") or (tempTab[1][0] == "O" and tempTab[1][1] == "O" and tempTab[1][2] == "O") or (tempTab[2][0] == "O" and tempTab[2][1] == "O" and tempTab[2][2] == "O") or (tempTab[0][0] == "O" and tempTab[1][0] == "O" and tempTab[2][0] == "O") or (tempTab[0][1] == "O" and tempTab[1][1] == "O" and tempTab[2][1] == "O") or (tempTab[0][2] == "O" and tempTab[1][2] == "O" and tempTab[2][2] == "O") or (tempTab[0][0] == "O" and tempTab[1][1] == "O" and tempTab[2][2] == "O") or (tempTab[0][2] == "O" and tempTab[1][1] == "O" and tempTab[2][0] == "O"):
-                            tab[x][y] = "X"
-                            break
-        else:
-            if tab[2][2] == "_":
+                if not check:
+                    for y in range(3):
+                        if tab[x][y] == "_":
+                            tempTab = copy.deepcopy(tab)
+                            tempTab[x][y] = "O"
+                            if (tempTab[0][0] == "O" and tempTab[0][1] == "O" and tempTab[0][2] == "O") or (tempTab[1][0] == "O" and tempTab[1][1] == "O" and tempTab[1][2] == "O") or (tempTab[2][0] == "O" and tempTab[2][1] == "O" and tempTab[2][2] == "O") or (tempTab[0][0] == "O" and tempTab[1][0] == "O" and tempTab[2][0] == "O") or (tempTab[0][1] == "O" and tempTab[1][1] == "O" and tempTab[2][1] == "O") or (tempTab[0][2] == "O" and tempTab[1][2] == "O" and tempTab[2][2] == "O") or (tempTab[0][0] == "O" and tempTab[1][1] == "O" and tempTab[2][2] == "O") or (tempTab[0][2] == "O" and tempTab[1][1] == "O" and tempTab[2][0] == "O"):
+                                tab[x][y] = "X"
+                                check = True
+                                break
+            if not check and tab[2][2] == "_":
                 tab[2][2] = "X"
-            else:
+            elif not check:
                 tab[2][0] = "X"
         
-        if action == 6:
+        if action == 7:
+            check = False
             for x in range(3):
-                for y in range(3):
-                    if tab[x][y] == "_":
-                        tempTab = tab
-                        tempTab[x][y] = "X"
-                        if (tempTab[0][0] == "X" and tempTab[0][1] == "X" and tempTab[0][2] == "X") or (tempTab[1][0] == "X" and tempTab[1][1] == "X" and tempTab[1][2] == "X") or (tempTab[2][0] == "X" and tempTab[2][1] == "X" and tempTab[2][2] == "X") or (tempTab[0][0] == "X" and tempTab[1][0] == "X" and tempTab[2][0] == "X") or (tempTab[0][1] == "X" and tempTab[1][1] == "X" and tempTab[2][1] == "X") or (tempTab[0][2] == "X" and tempTab[1][2] == "X" and tempTab[2][2] == "X") or (tempTab[0][0] == "X" and tempTab[1][1] == "X" and tempTab[2][2] == "X") or (tempTab[0][2] == "X" and tempTab[1][1] == "X" and tempTab[2][0] == "X"):
+                if not check:
+                    for y in range(3):
+                        if tab[x][y] == "_":
+                            tempTab = copy.deepcopy(tab)
+                            tempTab[x][y] = "X"
+                            if (tempTab[0][0] == "X" and tempTab[0][1] == "X" and tempTab[0][2] == "X") or (tempTab[1][0] == "X" and tempTab[1][1] == "X" and tempTab[1][2] == "X") or (tempTab[2][0] == "X" and tempTab[2][1] == "X" and tempTab[2][2] == "X") or (tempTab[0][0] == "X" and tempTab[1][0] == "X" and tempTab[2][0] == "X") or (tempTab[0][1] == "X" and tempTab[1][1] == "X" and tempTab[2][1] == "X") or (tempTab[0][2] == "X" and tempTab[1][2] == "X" and tempTab[2][2] == "X") or (tempTab[0][0] == "X" and tempTab[1][1] == "X" and tempTab[2][2] == "X") or (tempTab[0][2] == "X" and tempTab[1][1] == "X" and tempTab[2][0] == "X"):
+                                tab[x][y] = "X"
+                                check = True
+                                break
+            if not check:
+                check = False
+                for x in range(3):
+                    if not check:
+                        for y in range(3):
+                            if tab[x][y] == "_":
+                                tab[x][y] = "X"
+                                check = True
+                                break
+        
+        if action == 9:
+            check = False
+            for x in range(3):
+                if not check:
+                    for y in range(3):
+                        if tab[x][y] == "_":
                             tab[x][y] = "X"
+                            check = True
                             break
 
         cur = playerSymbole[curPlayerID]
@@ -75,9 +100,11 @@ def impossibleMorpion():
                 print(i)
             print(f"{curPlayer[curPlayerID]} a gagné")
             break
+        elif action == 9:
+            print("égalité...")
+            break
         
-        # if tab[choixX][choixY] == playerSymbole[curPlayerID]:
-        #     print("lol")
         curPlayerID = curPlayerID * -1
+
 
 impossibleMorpion()
